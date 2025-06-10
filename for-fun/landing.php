c3efb68d0822b529f63d657de9ce262b8ecbab99
thatguy9358 <149450073+thatguy9358@users.noreply.github.com>
2025-06-09 20:39:22 -0400

    initial version

---
+++ for-fun/landing.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Charlie Hacks</title>
+    <link rel="stylesheet" href="/style.css">
+  </head>
+  <body>
+    <header>
+      <?php include '../header.php'; ?>
+    </header>
+    <div class="row">
+      <div class="column-sidebar">
+        <p>content here</p>
+        <p>content here</p>
+        <p>content here</p>
+        <p>content here</p>
+        <p>content here</p>
+      </div>
+      <div class="column-blog">
+        <h1>Side Projects</h1>
+        <p>A collection of different projects I've completed in an attempt to learn something new and do something fun</p>
+      </div>
+    </div>
---
+++ header.php
@@ -0,0 +1,8 @@
+<?php
+ echo
+"<div class=\"header\">
+      <a href=\"/index.php\">
+          <img src=\"/img/logo2.png\" alt=\"site logo\" style=\"width:100px;height:100px;\" alt=\"logo\" />
+      </a>
+    </div>";
+  ?>
